5503fae057197589e3e4d94a6f0cb73d82e2f342
Mitroshin <mitroshin.develop@gmail.com>
2017-03-19 19:05:18 +0300

    Добавлена информация о лицензии

--- app/src/main/java/com/devgmail/mitroshin/totutu/controllers/ListFragment.java
+++ app/src/main/java/com/devgmail/mitroshin/totutu/controllers/ListFragment.java
@@ -23,6 +23,22 @@ import com.devgmail.mitroshin.totutu.util.StationCursorAdapter;
 
 import static com.devgmail.mitroshin.totutu.hosts.ListActivity.EXTRA_DIRECTION_TYPE;
 
+/**
+ Copyright 2017 Mitroshin Dmitry (mitroshin.develop@gmail.com)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 //Контроллер для представления fragment_list.xml
 
 public class ListFragment extends Fragment implements AdapterView.OnItemClickListener {
